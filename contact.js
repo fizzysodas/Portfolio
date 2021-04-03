f2f680fe1ee263f451f3d1bcfd6e32f073285315
fizzysodas <66885605+fizzysodas@users.noreply.github.com>
2021-04-02 17:12:37 -0700

    done

---
+++ contact.js
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>My Portfolio</title>
+</head>
+<body>
+    <header>
+
+        <div id="mySidenav" class="sidenav">
+            <a href="#">About Me</a>
+            <a href="#">Projects</a>
+            <a href="#">Skills</a>
+            <a href="./contact.html">Contact</a>
+        </div>
+        <h2></h2>
+    </header>
+
+  <section id="contact">
+    <h2>Contact</h2>
+    <p>Have an interesting project? I would love to hear about it!</p>
+  <div class="item1">
+               <img id="sLogo" src="https://www.pngitem.com/pimgs/m/513-5138976_transparent-background-black-social-media-icons-png-png.png" alt="">
+        </div>
+  </section>
+    
+</body>
+</html>
